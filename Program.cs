bb57b5fba2e5a6561af4d717c377fd2ebe451b30
WaToI <kouhouhaiki@gmail.com>
2020-04-12 21:53:53 +0900

    update

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace efMysql
+{
+    using System;
+    using System.Linq;
+    using System.IO;
+    using Microsoft.Extensions.Configuration;
+    using efMysql.Models;
+
+    class Program
+    {
+        static IConfiguration GetConfiguration()
+        {
+            var configBuilder = new ConfigurationBuilder();
+            // 設定ファイルのベースパスをカレントディレクトリ( 実行ファイルと同じディレクトリ )にします。
+            configBuilder.SetBasePath(Directory.GetCurrentDirectory());
+            // Json ファイルへのパスを設定します。SetBasePath() で設定したパスからの相対パスになります。
+            configBuilder.AddJsonFile(@"appsettings.json");
+            return configBuilder.Build();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("start");
+            
+            using(var db = new Context()){
+                var users = db.User.Where(w=>w.User1=="root");
+                foreach(var i in users){
+                    Console.WriteLine($"{i.Host} {i.User1}");
+                }
+            }
+        }
+    }
+}
